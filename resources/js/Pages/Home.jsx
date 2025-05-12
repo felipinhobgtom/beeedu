647ba1d766a91ed56676af40f915b9e8f48e41fb
felipinhobgtom <felipinhobgouveia@gmail.com>
2025-05-11 21:54:24 -0300

    chore: more tests, inertia is now ready to use with react

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,6 +1,9 @@
+import { Header } from "../Components/Header/Header";
+
 export default function Home() {
     return (
         <>
+            <Header />
             <h1>Hello World!</h1>
         </>
     )
